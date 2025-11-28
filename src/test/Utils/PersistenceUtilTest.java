fd7e2071da0b2c3debc0011e37b99a9ba9f15914
some1less <muracks.max@gmail.com>
2025-11-28 01:20:21 +0100

    Refactored: improved correct saving and loading + implemented test for handling exception while saving

--- src/test/Utils/PersistenceUtilTest.java
+++ src/test/Utils/PersistenceUtilTest.java
@@ -1,20 +1,23 @@
 package test.Utils;
 
+import main.Editions.LimitedEdition;
 import main.Enums.ClothingSize;
+import main.Footwear.Footwear;
 import main.PersistenceModels.Boot;
+import main.PersistenceModels.DebitCard;
 import main.PersistenceModels.Hoodie;
 import main.PersistenceModels.PersistenceUtil;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PersistenceUtilTest {
 
     private static final String FILE_PATH = "data.ser";
@@ -26,21 +29,26 @@ class PersistenceUtilTest {
     }
 
     @Test
+    @Order(0)
     void saveAll_shouldCreateFile() {
         PersistenceUtil.saveAll();
         assertTrue(dataFile.exists(), "File has to be created");
     }
 
     @Test
+    @Order(1)
     void saveAll_shouldActuallySaveObject(){
         new Hoodie("Test", "Brand", 41, 1, List.of("Test1"), List.of("Test1"), ClothingSize.S, true);
         PersistenceUtil.saveAll();
 
+        List<Hoodie> loadedHoodieList = Hoodie.getExtent();
+        System.out.println("Hoodie list size: " + loadedHoodieList.size());
         assertTrue(dataFile.length() > 0, "File is not empty => object is saved successfully");
     }
 
     @Test
-    void loadAll_shouldRestoreSavedObjects() {
+    @Order(2)
+    void loadAll_shouldSaveAndLoadObject() {
         new Boot("Testing", "Test", 61.0,5,List.of("test1"), List.of("test2"), true,37.0);
 
         PersistenceUtil.saveAll();
@@ -51,12 +59,31 @@ class PersistenceUtilTest {
 
     }
 
+    @Test
+    @Order(3)
+    void saveObject_withNonSerializableField() {
+
+        new Hoodie("Poison", "Test", 100, 0, List.of("em1"), List.of("em1"), ClothingSize.S, true) {
+
+            private final Thread nonSerializableField = new Thread();
+        };
+
+        PersistenceUtil.saveAll();
+
+        // catching error
+//        assertNotNull(PersistenceUtil.lastError);
+        assertInstanceOf(IOException.class, PersistenceUtil.lastError);
+        System.out.println("Error was caught: " + PersistenceUtil.lastError.getMessage());
+
+    }
+
     private void clearMemory(){
         try{
             Files.deleteIfExists(dataFile.toPath());
-//            PersistenceUtil.loadAll();
+            PersistenceUtil.loadAll();
         } catch (IOException e){
             System.err.println("Error while clearing in test: " + e.getMessage());
         }
     }
+
 }
